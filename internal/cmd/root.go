5d5050fed2d3d4febb2052eed13dc4aaa7c9ca04
Kevin McDermott <bigkevmcd@gmail.com>
2020-10-09 07:00:40 +0100

    Refactor the cobra setup.
    Split out the SMTP command and allow configuration of the Maildir
    storage directory.

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -13,7 +13,8 @@ import (
 )
 
 const (
-	portFlag = "port"
+	portFlag    = "port"
+	maildirFlag = "maildir"
 )
 
 func init() {
@@ -24,14 +25,23 @@ func initConfig() {
 	viper.AutomaticEnv()
 }
 
-func makeHTTPCmd() *cobra.Command {
+func makeRootCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "john",
-		Short: "Simple SMTP server with Mailets",
+		Short: "Simple Mailet processing service.",
+	}
+	cmd.AddCommand(makeSMTPServerCmd())
+	return cmd
+}
+
+func makeSMTPServerCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "smtp",
+		Short: "Act as an SMTP server and process incoming emails",
 		Run: func(cmd *cobra.Command, args []string) {
 			listen := fmt.Sprintf(":%d", viper.GetInt(portFlag))
 			log.Printf("listening on %s", listen)
-			h, err := maildir.New("./tmp")
+			h, err := maildir.New(viper.GetString(maildirFlag))
 			logIfError(err)
 			logIfError(smtpd.ListenAndServe(listen, handler.MakeHandler(h), "John SMTP", ""))
 		},
@@ -43,12 +53,19 @@ func makeHTTPCmd() *cobra.Command {
 		"port to receive mail on",
 	)
 	logIfError(viper.BindPFlag(portFlag, cmd.Flags().Lookup(portFlag)))
+
+	cmd.Flags().String(
+		maildirFlag,
+		"./tmp",
+		"Path to store Maildir mail",
+	)
+	logIfError(viper.BindPFlag(maildirFlag, cmd.Flags().Lookup(maildirFlag)))
 	return cmd
 }
 
 // Execute is the main entry point into this component.
 func Execute() {
-	logIfError(makeHTTPCmd().Execute())
+	logIfError(makeRootCmd().Execute())
 }
 
 func logIfError(e error) {
